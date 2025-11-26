ef4154b7899e0e303445a5f60f7f60a015b8d6e7
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-11-26 15:30:12 -0500

    ok

--- src/app/pages/practicesong/practicesong.ts
+++ src/app/pages/practicesong/practicesong.ts
@@ -31,9 +31,13 @@ export class Practicesong extends CommonSpeech {
   ];
 
   song: Verse[] = [
-    { txt: "Yeah, you could be the greatest", },
+    { txt: "you could be the greatest", },
     { txt: "you can be the best", },
-    { txt: "you can be the king kong banging on your chest", },
+    { txt: "you can be the king kong bangin on your chest", },
+    { txt: "you can beat the world", },
+    { txt: "you can beat the war", },
+    { txt: "you can talk to God", },
+    { txt: "go bangin on his door", },
   ];
 
   constructor(
