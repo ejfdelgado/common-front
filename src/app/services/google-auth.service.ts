03e44503ae1ec09d00f2117393621f39876a4457
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-01-08 08:04:40 -0500

    ok

--- src/app/services/google-auth.service.ts
+++ src/app/services/google-auth.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, signal, computed, effect, ChangeDetectorRef, NgZone } from '@angular/core';
 import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { GoogleGsiLoaderService } from './google-gsi-loader.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { jwtDecode } from 'jwt-decode';
 import { environment } from 'environments/environment';
 
 const AUTH_FLAG_KEY = 'google_auth_logged_in';
@@ -19,30 +17,22 @@ declare global {
 export class GoogleAuthService {
     private readonly clientId = environment.googleClientId;
 
-    /* ---------------- Signals (internal state) ---------------- */
-
     private userSignal = signal<GoogleUser | null>(null);
 
     readonly user = computed(() => this.userSignal());
     readonly isLoggedIn = computed(() => !!this.userSignal());
 
-    /* ---------------- RxJS Observer API ---------------- */
-
     private authStateSubject = new BehaviorSubject<GoogleUser | null>(null);
 
-    /** Observable for subscribers */
     readonly authState$: Observable<GoogleUser | null> =
         this.authStateSubject.asObservable();
 
-    private tokenClient!: any;
     private accessToken?: string;
 
     constructor(
         private loader: GoogleGsiLoaderService,
-        private http: HttpClient,
         private zone: NgZone,
     ) {
-        /* Bridge Signal → RxJS */
         effect(() => {
             this.authStateSubject.next(this.userSignal());
         });
@@ -53,19 +43,6 @@ export class GoogleAuthService {
         return this.accessToken;
     }
 
-    isTokenExpired(token: string) {
-        try {
-            const decoded = jwtDecode<any>(token);
-            const currentTime = Date.now() / 1000;
-            return decoded.exp < currentTime;
-        } catch (error) {
-            console.log(error);
-            return true;
-        }
-    }
-
-    /* ---------------- OAuth Setup ---------------- */
-
     private async initialize(): Promise<void> {
         await this.loader.load();
 
@@ -75,38 +52,16 @@ export class GoogleAuthService {
             auto_select: false
         });
 
-        this.tokenClient = window.google.accounts.oauth2.initTokenClient({
-            client_id: this.clientId,
-            scope: 'openid profile email',
-            callback: (response: any) => {
-                if (response.access_token) {
-                    this.accessToken = response.access_token;
-                    localStorage.setItem(AUTH_FLAG_KEY, 'true');
-                    this.zone.run(() => {
-                        this.fetchUserInfo(response.access_token).subscribe();
-                    });
-                }
-            },
-        });
-
         if (localStorage.getItem(AUTH_FLAG_KEY) === 'true') {
             this.silentLogin();
         }
     }
 
     private silentLogin(): void {
-        /*
-        this.tokenClient?.requestAccessToken({
-            prompt: 'none'
-        });
-        */
         window.google.accounts.id.prompt();
     }
 
-    /* ---------------- Public API ---------------- */
-
     login(): void {
-        //this.tokenClient.requestAccessToken({ prompt: 'consent' });
         window.google.accounts.id.prompt();
     }
 
@@ -116,7 +71,6 @@ export class GoogleAuthService {
             localStorage.removeItem(AUTH_FLAG_KEY);
             this.userSignal.set(null);
             window.google.accounts.id.disableAutoSelect();
-            //window.google.accounts.oauth2.revoke(this.clientId, () => { });
         });
     }
 
@@ -135,33 +89,6 @@ export class GoogleAuthService {
             token: idToken
         });
     }
-
-    /* ---------------- Internal ---------------- */
-
-    private fetchUserInfo(accessToken: string): Observable<GoogleUser> {
-        return this.http
-            .get<any>('https://www.googleapis.com/oauth2/v3/userinfo', {
-                headers: new HttpHeaders({
-                    Authorization: `Bearer ${accessToken}`,
-                }),
-            })
-            .pipe(
-                map((user) => {
-                    return {
-                        id: user.sub,
-                        email: user.email,
-                        name: user.name,
-                        token: accessToken,
-                        picture: user.picture
-                    };
-                }),
-                tap((user: GoogleUser) => {
-                    // fire your signal after mapping
-                    this.userSignal.set(user);
-                }),
-            );
-    }
-
 }
 
 export interface GoogleUser {
