9032d945c8ae76f841a253dc5606917a104aa084
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-01-13 09:22:22 -0500

    map placed

--- src/app/pages/voyage-photo/voyage-photo.ts
+++ src/app/pages/voyage-photo/voyage-photo.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { CameraCaptureComponent } from '@components/camera-capture/camera-capture';
+import { MarkType, SimpleMapComponent } from '@components/simple-map/simple-map';
+import { FileService } from '@services/file.srv';
+import { FirestoreService } from '@services/firestore.service';
+import { GoogleAuthService } from '@services/google-auth.service';
+import { IndicatorService } from '@services/indicator.service';
+import { LocationService } from '@services/location.service';
 
 @Component({
   selector: 'app-voyage-photo',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    CameraCaptureComponent,
+    SimpleMapComponent,
+  ],
   templateUrl: './voyage-photo.html',
   styleUrl: './voyage-photo.scss',
 })
 export class VoyagePhoto {
 
+  @ViewChild("simple_map") simpleMap!: SimpleMapComponent;
+
+  constructor(
+    private indicatorSrv: IndicatorService,
+    public authSrv: GoogleAuthService,
+    private http: HttpClient,
+    private fileSrv: FileService,
+    private firestoreSrv: FirestoreService,
+    public cdr: ChangeDetectorRef,
+    public locationSrv: LocationService,
+  ) {
+    this.authSrv.authState$.subscribe(user => {
+      if (user) {
+        console.log('Logged in:', user.name);
+      } else {
+        console.log('Logged out');
+      }
+    });
+  }
+
+  async transformMark(data: MarkType) {
+    return `<b class="white_subtitle" style="font-size: 2em;">📍</b>`;
+  }
+
+  async addMark() {
+    const activity = this.indicatorSrv.start();
+    try {
+      const pos = await this.locationSrv.getCurrentPosition();
+      const marker: MarkType = {
+        id: "",
+        lat: pos.latitude,
+        lon: pos.longitude,
+        title: new Date().toDateString(),
+      };
+      const observable = await this.simpleMap.addMarker(marker);
+      observable.subscribe((mark) => {
+        console.log(mark);
+      });
+    } catch (err) { }
+    activity.done();
+  }
 }
