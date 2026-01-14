d164f5dfc1a0377b1a78ea7745b5a7147e7413df
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-01-14 17:20:59 -0500

    ok

--- src/app/services/google-auth.service.ts
+++ src/app/services/google-auth.service.ts
@@ -155,6 +155,8 @@ export class GoogleAuthService {
             this.handleCredential({
                 credential: oldIdToken
             });
+        } else {
+            this.logout();
         }
     }
 
