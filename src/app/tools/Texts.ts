99963365192d03d2375cb7ecfa7cac3b7b706c2b
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-02-23 14:49:35 -0500

    ok

--- src/app/tools/Texts.ts
+++ src/app/tools/Texts.ts
@@ -3,3 +3,7 @@ export function removeAccents(text: string): string {
         .normalize('NFD')                 // Separates characters from their accents
         .replace(/[\u0300-\u036f]/g, ''); // Removes the accent marks (combining marks)
 };
+
+export function normalizeName(name: string) {
+    return removeAccents(name.toLowerCase()).replace(/^[a-z]/g, "_");
+}
