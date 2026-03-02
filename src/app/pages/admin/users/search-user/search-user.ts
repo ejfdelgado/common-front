13c2938b199251e6c55cabf629c72bc48e4bac98
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-03-02 13:57:01 -0500

    ok

--- src/app/pages/admin/users/search-user/search-user.ts
+++ src/app/pages/admin/users/search-user/search-user.ts
@@ -1,9 +1,10 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { SearchInputComponent } from '@components/search-input/search-input';
 import { UsersService } from '@services/users.service';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-search-user',
@@ -19,9 +20,12 @@ import { UsersService } from '@services/users.service';
 })
 export class SearchUser {
 
+  usersMatched: User[] = [];
+
   constructor(
     private dialogRef: MatDialogRef<SearchUser>,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    public cdr: ChangeDetectorRef,
     public userSrv: UsersService,
   ) {
 
@@ -31,7 +35,21 @@ export class SearchUser {
     this.dialogRef.close();
   }
 
-  searchUser(text: string) {
+  async searchUser(text: string) {
+    if (text.length == 0) {
+      this.usersMatched = [];
+    } else {
+      const response = await this.userSrv.pageUsers({ limit: 5, email: text });
+      if (response.success) {
+        this.usersMatched = response.data.list;
+      } else {
+        this.usersMatched = [];
+      }
+      this.cdr.detectChanges();
+    }
+  }
 
+  selectUser(user: User) {
+    this.dialogRef.close(user);
   }
 }
