61230f3f9b5be98f92466941cd166cebf4303543
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-07 21:22:03 -0500

    advance in the terrain height

--- src/app/pages/threejstest/threejs/controllers/TerrainElevationController.ts
+++ src/app/pages/threejstest/threejs/controllers/TerrainElevationController.ts
@@ -1,10 +1,25 @@
 import { AvatarBodyEvent, ControllerUpdateResponse } from "@mytypes/bodyTypes";
 import { SceneControllerAbstract } from "../SceneControllerAbstract";
+import * as THREE from 'three';
 
 export class TerrainElevationController extends SceneControllerAbstract {
 
+    transformationMatrix: THREE.Matrix4 = new THREE.Matrix4().identity();
+
     override async update(): Promise<ControllerUpdateResponse> {
-        return {};
+        const state = this.scene.avatarState;
+        const { positionX, positionZ } = state;
+        // TODO Given the x,z coordinates, must ray cast vertically the terrain
+        let yTerrain = 0;
+        // Then get the height of the terrain and set the matrix
+        this.transformationMatrix = new THREE.Matrix4().makeTranslation(
+            0,
+            yTerrain,
+            0,
+        );
+        return {
+            avatarTransform: this.transformationMatrix,
+        };
     }
 
     override async stop(): Promise<void> {
