1d75792135dffc42b01eb58c93d0bd72923be3b8
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-12 14:38:05 -0500

    correct class running in the main thread

--- src/app/avatar/workers/ControllerExecutor.ts
+++ src/app/avatar/workers/ControllerExecutor.ts
@@ -1,68 +1,7 @@
+import { AbstractControllerExecutor } from "./AbstractControllerExecutor";
 
-export class ControllerExecutor {
-    pending = new Map();
-    worker!: Worker;
-    ready!: Promise<void>;
-    isReady: boolean = false;
-
-    constructor() {
-        this.warmUp();
-    }
-
-    async warmUp() {
-        this.worker = new Worker(
-            new URL('./controllers.worker', import.meta.url),
-            { type: 'module' }
-        );
-        this.ready = new Promise((resolve, reject) => {
-            this.worker.onmessage = async (event) => {
-                const { type, payload, id, success } = event.data;
-                if (type === 'READY') {
-                    this.isReady = true;
-                    resolve();
-                } else if (type == "RESPONSE_WORKER") {
-                    if (success === true) {
-                        this.pending.get(id)?.resolve(payload);
-                    } else {
-                        this.pending.get(id)?.reject(payload);
-                    }
-                    this.pending.delete(id);
-                } else if (type == "CALL_MAIN") {
-                    const { funName, argument } = (payload as any);
-                    try {
-                        const result = await (this as any)[funName](argument);
-                        this.worker.postMessage({
-                            id,
-                            type: 'RESPONSE_MAIN',
-                            payload: result,
-                            success: true,
-                        });
-                    } catch (err) {
-                        this.worker.postMessage({
-                            id,
-                            type: 'RESPONSE_MAIN',
-                            payload: err,
-                            success: false,
-                        });
-                    }
-                }
-            };
-            this.worker.onerror = (err: any) => {
-                this.isReady = false;
-                reject(err);
-            }
-        });
-    }
-
-    async callWorker(funName: string, argument: any = {}) {
-        return new Promise((resolve, reject) => {
-            const id = crypto.randomUUID();
-            this.pending.set(id, { resolve, reject });
-            const payload = { funName, argument };
-            this.worker.postMessage({ id, type: 'CALL_WORKER', payload });
-        });
-    }
-
+export class ControllerExecutor extends AbstractControllerExecutor {
+    
     async getSome(arg: number) {
         return arg * 100;
     }
