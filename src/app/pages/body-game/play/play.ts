52966b7f2973fd0cc055a1b5afd3bc06cf2b66e0
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-14 15:59:20 -0500

    added play page

--- src/app/pages/body-game/play/play.ts
+++ src/app/pages/body-game/play/play.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { SideMenu } from '@components/side-menu/side-menu';
+import { Statusbar } from '@components/statusbar/statusbar';
+import { MenuOptionType } from '@mytypes/StatusBar';
 
 @Component({
   selector: 'app-play',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    Statusbar,
+    SideMenu
+  ],
   templateUrl: './play.html',
   styleUrl: './play.scss',
 })
 export class PlayComponent {
+  menuOptions: MenuOptionType[] = [];
 
 }
