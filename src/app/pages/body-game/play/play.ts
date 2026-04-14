3f11dd87a2757c8c007b509acac3c5aa71c4ab2f
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-14 16:33:29 -0500

    ok

--- src/app/pages/body-game/play/play.ts
+++ src/app/pages/body-game/play/play.ts
@@ -9,6 +9,15 @@ import { MenuOptionType } from '@mytypes/StatusBar';
 import { AuthService } from '@services/auth.service';
 import { FullscreenService } from '@services/fullscreen.service';
 import { BodyTracker } from './components/body-tracker/body-tracker';
+import { RoomGameType } from '@mytypes/ActionGameTypes';
+import { UINotificationSrv } from '@services/uinotifications.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SharedWith } from 'app/pages/admin/users/shared-with/shared-with';
+import { getUrlQueryParams } from '@tools/UrlUtil';
+import { FirestoreService } from '@services/firestore.service';
+import { AssistantDataType } from '@mytypes/ragTypes';
+
+const MODEL_NAME_PARENT = "room-private";
 
 @Component({
   selector: 'app-play',
@@ -25,6 +34,7 @@ import { BodyTracker } from './components/body-tracker/body-tracker';
 export class PlayComponent extends AuthenticatedComponent implements OnInit, OnDestroy {
 
   menuOptions: MenuOptionType[] = [];
+  room: RoomGameType | null = null;
 
   constructor(
     public override sanitizer: DomSanitizer,
@@ -32,12 +42,64 @@ export class PlayComponent extends AuthenticatedComponent implements OnInit, OnD
     public override authSrv: AuthService,
     public override cdr: ChangeDetectorRef,
     //
+    private uiNotificationSrv: UINotificationSrv,
+    private dialog: MatDialog,
+    private firestoreSrv: FirestoreService,
   ) {
     super(sanitizer, fullScreenSrv, authSrv, cdr);
+
+    this.menuOptions.push({
+      label: "Permissions",
+      icon: "lock",
+      children: [],
+      callback: () => {
+        this.openPermissions();
+      },
+    });
   }
 
-  ngOnInit(): void {
 
+  async openPermissions() {
+    if (!this.room) {
+      return;
+    }
+    const dialogRef = this.dialog.open(SharedWith, {
+      width: '800px',
+      panelClass: 'custom-emoji-picker',
+      autoFocus: !this.isMobile(),
+      data: {
+        id: this.room.id,
+        collection: MODEL_NAME_PARENT,
+        mode: "normal",
+      },
+    });
+    dialogRef.afterClosed().subscribe(async (result) => {
+      //console.log(result);
+    });
+  }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      await this.loadCollection();
+    } catch (err: any) {
+      this.uiNotificationSrv.show(err.message);
+    }
+  }
+
+  async loadCollection() {
+    const params = getUrlQueryParams();
+    const col = params.get("col");
+    const id = params.get("id");
+    if (col && id) {
+      const temp = await this.firestoreSrv.readById(col, id);
+      if (temp) {
+        this.room = temp as AssistantDataType;
+        document.title = this.room.title;
+      } else {
+        this.room = null;
+      }
+      this.cdr.detectChanges();
+    }
   }
 
   ngOnDestroy(): void {
