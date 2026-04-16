42ed80cdd1c803b91cb449d54d70773db929d8bb
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-16 17:35:17 -0500

    sending binary data ok

--- src/app/pages/body-game/experiments/media-pipe-hand/media-pipe-hand.ts
+++ src/app/pages/body-game/experiments/media-pipe-hand/media-pipe-hand.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { defaultFirebaseApp } from '@services/firebase';
-import { joinRoom, Room } from '@trystero-p2p/firebase';
+import { ActionReceiver, ActionSender, DataPayload, joinRoom, Room } from '@trystero-p2p/firebase';
 import { encode, decode } from "@msgpack/msgpack";
 
 const appId = 'ejfexperiments';
@@ -13,6 +13,9 @@ const appId = 'ejfexperiments';
 })
 export class MediaPipeHand implements AfterViewInit {
   room: Room | null = null;
+  sendBinaryData: ActionSender<DataPayload> | null = null;
+  localStream: MediaStream | null = null;
+
   ngAfterViewInit(): void {
     this.connectToRoom();
   }
@@ -22,27 +25,45 @@ export class MediaPipeHand implements AfterViewInit {
       firebaseApp: defaultFirebaseApp,
       appId,
     }, 'room-1');
-    // Detect peers joining/leaving
+
+    const [sendBinaryData, receiveBinaryData] = this.room.makeAction('binary-data');
+    this.sendBinaryData = sendBinaryData;
+    this.listenBinaryData(receiveBinaryData);
+
     this.room.onPeerJoin((peerId) => {
-      console.log('peer joined:', peerId)
+      console.log('peer joined:', peerId);
+      if (this.localStream) {
+        this.room!.addStream(this.localStream, peerId);
+      }
     });
     this.room.onPeerLeave((peerId) => {
-      console.log('peer left:', peerId)
+      console.log('peer left:', peerId);
+    });
+    this.room.onPeerStream((stream, peerId) => {
+      console.log('received audio stream from:', peerId);
+      const audio = new Audio();
+      audio.srcObject = stream;
+      audio.play();
     });
   }
 
   async startVoiceCall() {
-    // Here the logic
+    if (!this.room) return;
+    this.localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    this.room.addStream(this.localStream);
   }
 
   async broadcastBinaryData() {
+    if (!this.sendBinaryData) return;
     const sample = { key: "some data" };
     const encoded = encode(sample);
-    const blob = new Blob([encoded], { type: 'text/plain' });
+    await this.sendBinaryData(encoded);
   }
 
-  async listenBinaryData() {
-    // Must listen all except the originator
-    // Use: const model = decode(received);
+  listenBinaryData(receiveBinaryData: ActionReceiver<DataPayload>) {
+    receiveBinaryData((data, peerId) => {
+      const model = decode(data as Uint8Array);
+      console.log('received binary data from:', peerId, model);
+    });
   }
 }
