4d63c4f9c348aec06a7a584919b24a537f9f2e71
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-04-16 17:20:35 -0500

    ok

--- src/app/pages/body-game/experiments/media-pipe-hand/media-pipe-hand.ts
+++ src/app/pages/body-game/experiments/media-pipe-hand/media-pipe-hand.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { defaultFirebaseApp } from '@services/firebase';
-import { joinRoom } from '@trystero-p2p/firebase';
+import { joinRoom, Room } from '@trystero-p2p/firebase';
 
 const appId = 'ejfexperiments';
 
@@ -11,30 +11,22 @@ const appId = 'ejfexperiments';
   styleUrl: './media-pipe-hand.scss',
 })
 export class MediaPipeHand implements AfterViewInit {
-
+  room: Room | null = null;
   ngAfterViewInit(): void {
-    this.main();
+    this.connectToRoom();
   }
 
-  async main() {
-    const room = await joinRoom({
+  async connectToRoom() {
+    this.room = await joinRoom({
       firebaseApp: defaultFirebaseApp,
       appId,
     }, 'room-1');
-
-    // Send/receive actions (typed messages between peers)
-    const [sendHello, getHello] = room.makeAction('hello');
-
-    // Listen for incoming messages
-    getHello((data, peerId) => {
-      console.log(`Got hello from ${peerId}:`, data);
-    });
-
-    // Send to all peers in the room
-    sendHello({ text: 'hi everyone!' });
-
     // Detect peers joining/leaving
-    room.onPeerJoin(peerId => console.log('peer joined:', peerId));
-    room.onPeerLeave(peerId => console.log('peer left:', peerId));
+    this.room.onPeerJoin((peerId) => {
+      console.log('peer joined:', peerId)
+    });
+    this.room.onPeerLeave((peerId) => {
+      console.log('peer left:', peerId)
+    });
   }
 }
